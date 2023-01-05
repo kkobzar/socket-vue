41d33b1160a7b2955844242b1d74d5ab584aadbe
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-01-05 19:44:28 +0200

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const PORT = 3030
+
+app.use('/',express.static('test-socket/dist/'))
+
+io.on('connection',socket=>{
+    socket.on('chat message', msg=>{
+        io.emit('chat message', msg)
+    })
+})
+
+http.listen(PORT, ()=>{
+    console.log(`Server on ${PORT}`)
+})
